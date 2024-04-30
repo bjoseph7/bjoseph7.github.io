bb2f30e5ba2173470e4099cde7e9ca997b369a85
bjoseph7 <beignetluva@gmail.com>
2024-04-30 14:41:48 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,13 +14,11 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-async function bubbleSort(array){
-    for (var i = 0; i < array.length - 1; i++){
-        for (var j = array.length - 1; j = i + 1; j++){
-            if (array[j].value < array[j].value - 1){
-                temp = array[j];
-                array[j] = array[j] - 1;
-                temp = array[j - 1];
+async function bubbleSort(array) {
+    for (var i = 0; i <= array.length - 1; i++) {
+        for(var j = 0; j <= array.length -1; j++) {
+            if (array[j].value > array[i].value) {
+                swap(array,i,j)
                 updateCounter(bubbleCounter);
                 await sleep();
             }
@@ -28,55 +26,39 @@ async function bubbleSort(array){
     }
 }
 // TODO 3: Implement quickSort
-async function quickSort(array, left, right){
-    partition = array[i] && array[j];
-    if (right - left){
-        index = partition(array, left, right);
-    } else if (left < (index - 1)){
-        quickSort(array, left, index - 1);
-    } else if (index < right){
-        quickSort(array, index, right);
-    }
-    var index = await partition(array, left, right);
-    if (left < index - 1){
-        await quickSort(array, left, index - 1);
-    } else if (right > index){
-        await quickSort(array, left, right);
+async function quickSort(array, left, right) {
+    if ((right - left) > 0) {
+        var index = await partition(array,left,right)
+        if (left < (index - 1)) {
+            quickSort(array, left, index - 1)
+        }
+        if (index < right) {
+            quickSort(array,index, right)
+        }
     }
 }
 
 // TODOs 4 & 5: Implement partition
-async function partition(array, left, right){
-    pivot = array[Math.floor((right + left)/2)].value;
-    while (left.value < right.pivot){
-        left ++;
-    }
-        while (right.value <= pivot.value){
-            right --;
+async function partition(array, left, right) {
+    var pivot = array[Math.floor((right + left)/2)].value
+    while (left < right) {
+        while (array[left].value < pivot) {
+            left++
         }
-            if (left < right){
-                swap(array, left, right);
-                updateCounter(quickCounter);
-                await sleep();
-            }
+        while (array[right].value > pivot) {
+            right--
+        }
+        if (left < right) {
+            swap(array,left,right)
+            updateCounter(quickCounter)
+            await sleep()
+        }
     }
-    return left + 1;
-   // while (left < right){
-        //while (array[left] < pivot[left++]){
-
-            //while (array[right] > pivot[right--]){
-               // if (left < right){
-                    //array[left] = array[right];
-               // }
-           // }
-        //}
-    //}
-   // return left + 1;
-//} 
-
+    return left + 1
+}
 // TODO 1: Implement swap
 function swap(array, i, j){
-    temp = array[i];
+    var temp = array[i];
     array[i] = array[j];
     array[j] = temp;
     drawSwap(array, i, j);
